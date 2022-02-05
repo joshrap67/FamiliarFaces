6f19d5310e77afc15696586269332e04d453cb1c
joshrap67 <ghosthippo67@gmail.com>
2022-02-04 21:41:13 -0500

    More upgrade stuff

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       theme: ThemeData(
           primarySwatch: Palette.colorSwatch,
